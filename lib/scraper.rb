bc5b87555153834d3fb405f387698eab5bdbc36d
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:30:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,23 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     
+    doc = Nokogiri::HTML(open(profile_url))
+    scraped_student = {}
+
+    doc.css(".social-icon-container a").each do |link|
+      normalized_link = link.attribute("href").text
+
+      scraped_student[:twitter] = normalized_link if normalized_link.include?("twitter")
+      scraped_student[:linkedin] = normalized_link if normalized_link.include?("linkedin")
+      scraped_student[:github] = normalized_link if normalized_link.include?("github")
+      scraped_student[:blog] = normalized_link if link.css("img").attribute("src").text.include?("rss")
+    end
+
+    scraped_student[:profile_quote] = doc.css(".profile-quote").text
+    scraped_student[:bio] = doc.css("bio-content .description-holder p").text
+
+    scraped_student
+
   end
 
 end
