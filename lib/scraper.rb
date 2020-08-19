10ffd3102385dfdc2e1e7001cb62fc7c6b7d89a9
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:28:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,23 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+
+     scraped_students = []
+
+    doc.css(".student-card").each do |student|
+      scraped_students << {
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student-location").text,
+        profile_url: "#{student.css("a").attribute("href").value}"
+      }
+    end
+    scraped_students
     
   end
 
