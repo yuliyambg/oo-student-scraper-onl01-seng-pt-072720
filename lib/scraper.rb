40ba89d1d0c831bf44325dda3157538151778f67
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:43:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,24 +23,45 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     
-    doc = Nokogiri::HTML(open(profile_url))
-    scraped_student = {}
+  #   doc = Nokogiri::HTML(open(profile_url))
+  #   scraped_student = {}
 
-    doc.css(".social-icon-container a").each do |link|
-      normalized_link = link.attribute("href").text
+  #   doc.css(".social-icon-container a").each do |link|
+  #     normalized_link = link.attribute("href").text
 
-      scraped_student[:twitter] = normalized_link if normalized_link.include?("twitter")
-      scraped_student[:linkedin] = normalized_link if normalized_link.include?("linkedin")
-      scraped_student[:github] = normalized_link if normalized_link.include?("github")
-      scraped_student[:blog] = normalized_link if link.css("img").attribute("src").text.include?("rss")
-    end
+  #     scraped_student[:twitter] = normalized_link if normalized_link.include?("twitter")
+  #     scraped_student[:linkedin] = normalized_link if normalized_link.include?("linkedin")
+  #     scraped_student[:github] = normalized_link if normalized_link.include?("github")
+  #     scraped_student[:blog] = normalized_link if link.css("img").attribute("src").text.include?("rss")
+  #   end
 
-    scraped_student[:profile_quote] = doc.css(".profile-quote").text
-    scraped_student[:bio] = doc.css("bio-content .description-holder p").text
+  #   scraped_student[:profile_quote] = doc.css(".profile-quote").text
+  #   scraped_student[:bio] = doc.css("bio-content .description-holder p").text
 
-    scraped_student
+  #   scraped_student
   
-  end
+  # end
 
+
+social_icons = doc.css("div.social-icon-container a").collect {|x| x.attribute("href").value}
+    social_icons.each do |social_icon|
+      if social_icon.include?("linkedin")
+        student[:linkedin] = social_icon
+      elsif social_icon.include?("github")
+        student[:github] = social_icon
+      elsif social_icon.include?("twitter")
+        student[:twitter] = social_icon
+      else
+        student[:blog] = social_icon
+      end
+    end
+    # if doc.css("div.bio-content.content-holder div.description-holder p")
+      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+    # end
+    # if doc.css(".profile-quote")
+      student[:profile_quote] = doc.css(".profile-quote").text
+    # end
+    student
+  end
 end
 
