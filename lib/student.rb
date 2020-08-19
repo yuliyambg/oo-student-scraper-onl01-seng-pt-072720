5747b06e24016f12a42e6aa712f3279788288311
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:34:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      send("#{key}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
