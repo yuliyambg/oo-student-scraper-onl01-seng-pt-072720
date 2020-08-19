2eb0ea7d9ca689746d7dfc5a5f082d6d5c3ffcfa
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:38:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      Student.new(stude
+      Student.new(student)
   end
 
   def add_student_attributes(attributes_hash)
