8b97cee5cc4e8a7a856e1918ca2dca05af10205c
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-19 19:43:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,6 +24,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     
     doc = Nokogiri::HTML(open(profile_url))
+     student = Hash.new
   #   scraped_student = {}
 
   #   doc.css(".social-icon-container a").each do |link|
